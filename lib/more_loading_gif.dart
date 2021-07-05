341915c38e507bcdc2b9d0c17ae5df3a92cda40d
ajomuch92 <aaronmontes_92@yahoo.es>
2021-07-04 19:45:45 -0600

    Adding format and fixing bug to get the images from assets

--- lib/more_loading_gif.dart
+++ lib/more_loading_gif.dart
@@ -6,7 +6,8 @@ class MoreLoadingGif extends StatelessWidget {
   final MoreLoadingGifType type;
   final double? size;
 
-  const MoreLoadingGif({Key? key, required this.type, this.size = 40.0}) : super(key: key);
+  const MoreLoadingGif({Key? key, required this.type, this.size = 100.0})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -18,30 +19,29 @@ class MoreLoadingGif extends StatelessWidget {
   }
 
   Widget _getLoadingGif() {
-
     switch (this.type) {
       case MoreLoadingGifType.blocks:
-        return Image.asset('assets/Blocks.gif');
+        return Image.asset('packages/more_loading_gif/assets/Blocks.gif');
       case MoreLoadingGifType.chunck:
-        return Image.asset('assets/Chunk.gif');
+        return Image.asset('packages/more_loading_gif/assets/Chunk.gif');
       case MoreLoadingGifType.doubleRing:
-        return Image.asset('assets/DoubleRing.gif');
+        return Image.asset('packages/more_loading_gif/assets/DoubleRing.gif');
       case MoreLoadingGifType.eclipse:
-        return Image.asset('assets/Eclipse.gif');
+        return Image.asset('packages/more_loading_gif/assets/Eclipse.gif');
       case MoreLoadingGifType.ellipsis:
-        return Image.asset('assets/Ellipsis.gif');
+        return Image.asset('packages/more_loading_gif/assets/Ellipsis.gif');
       case MoreLoadingGifType.infinity:
-        return Image.asset('assets/Infinity.gif');
+        return Image.asset('packages/more_loading_gif/assets/Infinity.gif');
       case MoreLoadingGifType.magnify:
-        return Image.asset('assets/Magnify.gif');
+        return Image.asset('packages/more_loading_gif/assets/Magnify.gif');
       case MoreLoadingGifType.pulse:
-        return Image.asset('assets/Pulse.gif');
+        return Image.asset('packages/more_loading_gif/assets/Pulse.gif');
       case MoreLoadingGifType.ripple:
-        return Image.asset('assets/Ripple.gif');
+        return Image.asset('packages/more_loading_gif/assets/Ripple.gif');
       case MoreLoadingGifType.spin:
-        return Image.asset('assets/Spin.gif');
+        return Image.asset('packages/more_loading_gif/assets/Spin.gif');
       case MoreLoadingGifType.spinner:
-        return Image.asset('assets/Spinner.gif');
+        return Image.asset('packages/more_loading_gif/assets/Spinner.gif');
     }
   }
 }
